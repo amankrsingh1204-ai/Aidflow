5a9c772b0d2126d9e621e22926afba153b6ec465
Aman Kumar <amankrsingh1204@gmail.com>
2025-11-05 11:50:15 +0530

    Delete backend directory

--- backend/src/routes/recipient.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Placeholder for recipient routes
-router.get('/', async (req, res) => {
-    res.json({ message: 'Recipient routes to be implemented' });
-});
-
-module.exports = router;
